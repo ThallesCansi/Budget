6b3db6cdafc1543c83a3eea523f18b0a75fcf56a
vinicius-girelli <128396292+vinicius-girelli@users.noreply.github.com>
2023-06-28 19:28:29 +0000

    Dia da Apresentação

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ while option != 0:
     option = int(input("Options: "))
     if (option == 1):
         import os
-        os.system("uvicorn mainWeb:app --reload ")
+        os.system("uvicorn mainWeb:app --reload --host=0.0.0.0 --port=5000")
