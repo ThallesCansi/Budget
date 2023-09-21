63c5ff52dceb216de74d18c379a1601246d63b99
ThallesCansi <thalles_cansi@hotmail.com>
2023-09-21 17:26:04 +0000

    criação da tabela de transacoes

--- static/js/popup.js
+++ static/js/popup.js
@@ -1,33 +1,3 @@
-function openPopup() {
-    var popup = document.getElementById('app-form');
-    popup.style.display = 'flex';
-}
-
-function openPopupReceita() {
-    var popup = document.getElementById('app-form-receita');
-    popup.style.display = 'flex';
-}
-
-function openPopupDespesa() {
-    var popup = document.getElementById('app-form-despesa');
-    popup.style.display = 'flex';
-}
-
-function openPopupConta() {
-    var popup = document.getElementById('app-form-conta');
-    popup.style.display = 'flex';
-}
-
-function openPopupCategoria() {
-    var popup = document.getElementById('app-form-categoria');
-    popup.style.display = 'flex';
-}
-
-function openPopupExcluir() {
-    var popup = document.getElementById('app-form-excluir');
-    popup.style.display = 'flex';
-}
-
 var count = 0
 function openPopupButton() {
     if (count == 0) {
@@ -39,35 +9,4 @@ function openPopupButton() {
         popup.style.display = 'none';
         count = 0; 
     }
-}
-
-
-function closePopup() {
-    var popup = document.getElementById('app-form');
-    popup.style.display = 'none';
-}
-
-function closePopupReceita() {
-    var popup = document.getElementById('app-form-receita');
-    popup.style.display = 'none';
-}
-
-function closePopupDespesa() {
-    var popup = document.getElementById('app-form-despesa');
-    popup.style.display = 'none';
-}
-
-function closePopupConta() {
-    var popup = document.getElementById('app-form-conta');
-    popup.style.display = 'none';
-}
-
-function closePopupCategoria() {
-    var popup = document.getElementById('app-form-categoria');
-    popup.style.display = 'none';
-}
-
-function closePopupExcluir() {
-    var popup = document.getElementById('app-form-excluir');
-    popup.style.display = 'none';
 }
