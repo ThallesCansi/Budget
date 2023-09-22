41fc032a882148d7d4bc1cdde5798c23a3f64077
vinicius-girelli <128396292+vinicius-girelli@users.noreply.github.com>
2023-09-22 10:01:15 +0000

    Melhorias no script para redimensionar cabeçalho

--- static/js/larguraCabecalho.js
+++ static/js/larguraCabecalho.js
@@ -2,10 +2,10 @@ function calcularTamanhoDaTelaMenosElemento() {
     const larguraDaTela = window.innerWidth;
     const larguraDoElemento = document.getElementById('sidebar').offsetWidth;
     const tamanhoDaTelaMenosElemento = larguraDaTela - larguraDoElemento;
-    const amais = 50
+    var amais = 38
 
     // Aplicar a largura calculada à navbar
-    document.getElementById('navbar').style.width = tamanhoDaTelaMenosElemento + amais + 'px';
+    document.getElementById('navbar').style.width = tamanhoDaTelaMenosElemento  + amais + 'px';
     }
 
     // Chame a função após a carga completa da página
