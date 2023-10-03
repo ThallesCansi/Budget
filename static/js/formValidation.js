1bfd682fd3908c6d9d415a1fe9cbe8f1ba9de647
vinicius-girelli <viniciusgirelle@gmail.com>
2023-10-03 19:22:58 -0300

    Consertando formValidation

--- static/js/formValidation.js
+++ static/js/formValidation.js
@@ -1,77 +1,7 @@
 //ao carregar a página
 window.onload = function () {
-    formCadastro = document.querySelector("#formCadastro");
-    for (const [key, value] of new FormData(formCadastro)) {
-        campo = document.getElementById(key);
-        textoErro = campo.parentElement.querySelector("span");
-        if (textoErro) {
-            textoErro.textContent = "";
-            textoErro.classList.add("d-none");
-            campo.classList.remove("is-invalid");
-            campo.classList.remove("is-valid");
-        }
-    }
-
-    if (fieldValues) {
-        for (const [key, value] of Object.entries(fieldValues)) {
-            campo = document.getElementById(key);
-            campo.value = value;
-            campo.classList.add("is-valid");
-        }
-    }
-    
-    if (validationErrors) {
-        for (const [key, value] of Object.entries(validationErrors)) {
-            campo = document.getElementById(key);
-            textoErro = campo.parentElement.querySelector("span");
-            if (textoErro) {
-                textoErro.textContent = value[0];
-                textoErro.classList.remove("d-none");
-                campo.classList.remove("is-valid");
-                campo.classList.add("is-invalid");
-            }
-        }
-    }
-}
-
-window.onload = function () {
-    formLogin = document.querySelector("#formLogin");
-    for (const [key, value] of new FormData(formLogin)) {
-        campo = document.getElementById(key);
-        textoErro = campo.parentElement.querySelector("span");
-        if (textoErro) {
-            textoErro.textContent = "";
-            textoErro.classList.add("d-none");
-            campo.classList.remove("is-invalid");
-            campo.classList.remove("is-valid");
-        }
-    }
-
-    if (fieldValues) {
-        for (const [key, value] of Object.entries(fieldValues)) {
-            campo = document.getElementById(key);
-            campo.value = value;
-            campo.classList.add("is-valid");
-        }
-    }
-    
-    if (validationErrors) {
-        for (const [key, value] of Object.entries(validationErrors)) {
-            campo = document.getElementById(key);
-            textoErro = campo.parentElement.querySelector("span");
-            if (textoErro) {
-                textoErro.textContent = value[0];
-                textoErro.classList.remove("d-none");
-                campo.classList.remove("is-valid");
-                campo.classList.add("is-invalid");
-            }
-        }
-    }
-}
-
-window.onload = function () {
-    formCadastro = document.querySelector("#alterarSenha");
-    for (const [key, value] of new FormData(formCadastro)) {
+    form = document.querySelector("form");
+    for (const [key, value] of new FormData(form)) {
         campo = document.getElementById(key);
         textoErro = campo.parentElement.querySelector("span");
         if (textoErro) {
