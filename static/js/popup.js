8e244260ac5cbe423856e6ca8a183c32018e7f5c
ThallesCansi <thalles_cansi@hotmail.com>
2023-10-04 12:32:03 -0300

    Mudanças no Dashboard - botão popup

--- static/js/popup.js
+++ static/js/popup.js
@@ -1,12 +1,12 @@
-var count = 0
+var count = 0;
 function openPopupButton() {
     if (count == 0) {
-        var popup = document.getElementById('tipoRouD');
-        popup.style.display = 'flex';
-        count = 1; 
-    } else if (count == 1) {    
-        var popup = document.getElementById('tipoRouD');
-        popup.style.display = 'none';
-        count = 0; 
+        var popup = document.getElementById("tipoRouD");
+        popup.style.display = "flex";
+        count = 1;
+    } else if (count == 1) {
+        var popup = document.getElementById("tipoRouD");
+        popup.style.display = "none";
+        count = 0;
     }
 }
